5a7fc9cafa8273311fa6393e86b274ac570664e3
morgankn <118085617+morgankn@users.noreply.github.com>
2023-01-04 15:43:29 -0500

    added more jaascript

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 // need to make global variables
-var currentTime = dayjs().hour(12);
-var currentHour = dayjs().hour();
-$('#time').text(currentTime);
 
+var currentTime = dayjs().hour();
+var displayTime = dayjs.unix(Math.floor(Date.now() / 1000)).format('MMM D, YYYY, HH:mm:ss a');
+$('#time').text(displayTime);
 
 var presentEl = $('.row time-block present');
 var pastEl = $('.row time-block past');
 var futureEl = $('.row time-block future');
 var saveBtn = $('.saveBtn');
- 
+var value = [];
 
+//get local storage when page loads
+function getDailyInput(){
+  for(i = 0; i < 9; i++){
+    value.push(localStorage.getItem([i]));
+    $('#hour-' + [i]).text(value[i]);
+  }
+};
+getDailyInput();
+
+//set local storage when click btn
 $('.saveBtn').on('click',function(){
   var value = $(this).siblings('.description').val()
-  var id = $(this).parent().attr('id');
+  var id = $(this).siblings('.description').attr('id');
    localStorage.setItem(id, value);
   console.log(this)
-  // var data = 
 }
 )
 
-var info = localStorage.getItem('hour-9')
-$('#hour-9').children('.description').val(info);
+// var info = localStorage.getItem('hour-9')
+// $('#hour-9').children('.description').val(info);
 
 // text areas and inputs use value val() in Jquery 
 // .each loop 
@@ -36,6 +45,7 @@ var hourList = ['hour-9','' ]
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -55,3 +65,15 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+//color coordinate based on current time 
+$('.description').addClass('past')
+  for(i = 0; i <= 24; i++){
+    if (currentTime - 12 === i - 12){
+      $('#hour-' + [i-12]).addClass('present');
+    } else if (currentTime < i){
+      $('#hour-' + [i-12]).addClass('future');
+    }
+
+  };
+
