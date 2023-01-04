eb84c6c1ac2d0de1a826918ff1ea3d4729db1b52
morgankn <118085617+morgankn@users.noreply.github.com>
2023-01-04 16:48:47 -0500

    working on for loop

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var value = [];
 
 //get local storage when page loads
 function getDailyInput(){
-  for(i = 0; i < 9; i++){
+  for(i = 0; i < 12; i++){
     value.push(localStorage.getItem([i]));
     $('#hour-' + [i]).text(value[i]);
   }
@@ -35,36 +35,9 @@ $('.saveBtn').on('click',function(){
 // .each loop 
 
 var hourList = ['hour-9','' ]
-// .split - array of hour and nine 
 
-// compare to day.js .hour 
 
-// Change hours to 24 format loop through, concat 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding text area elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+ 
 
 //color coordinate based on current time 
 $('.description').addClass('past')
@@ -76,4 +49,3 @@ $('.description').addClass('past')
     }
 
   };
-
