5c4a8b72ffdbc817afd9a395e8e90381135a82ad
morgankn <118085617+morgankn@users.noreply.github.com>
2023-01-04 21:40:46 -0500

    added README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# work-scheduler-
+# work-scheduler
+
+## Description
+
+This application is for making a scheduler by the hour for the users workday. The day starts at 9 am and goes to 7 pm with textboxes for each time that lets the user input their own schedule and items to do for each hour. When the save button is clicked the information is then saved to local storage. The current time is at the top of the browser and the hours that are in the past are in grey while the current hour is red and the future hours are green.
+
+
+## Installation
+
+N/A
+
+## Usage
+Assets/Screen Shot 2023-01-04 at 6.28.55 PM.png
+Assets/Screen Shot 2023-01-04 at 6.29.03 PM.png
+Assets/Screen Shot 2023-01-04 at 9.34.11 PM.png
+
+## License
+
+N/A
+
+
+
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// need to make global variables
 
 var currentTime = dayjs().hour();
 var displayTime = dayjs.unix(Math.floor(Date.now() / 1000)).format('MMM D, YYYY, HH:mm:ss a');
@@ -10,7 +9,6 @@ var futureEl = $('.row time-block future');
 var saveBtn = $('.saveBtn');
 var value = [];
 
-//get local storage when page loads
 function getDailyInput(){
   for(i = 0; i < 12; i++){
     value.push(localStorage.getItem([i]));
@@ -19,7 +17,7 @@ function getDailyInput(){
 };
 getDailyInput();
 
-//set local storage when click btn
+
 $('.saveBtn').on('click',function(){
   var value = $(this).siblings('.description').val()
   var id = $(this).siblings('.description').attr('id');
@@ -28,18 +26,12 @@ $('.saveBtn').on('click',function(){
 }
 )
 
-// var info = localStorage.getItem('hour-9')
-// $('#hour-9').children('.description').val(info);
-
-// text areas and inputs use value val() in Jquery 
-// .each loop 
 
 var hourList = ['hour-9','' ]
 
 
  
 
-//color coordinate based on current time 
 $('.description').addClass('past')
   for(i = 0; i <= 24; i++){
     if (currentTime - 12 === i - 12){
@@ -47,5 +39,4 @@ $('.description').addClass('past')
     } else if (currentTime < i){
       $('#hour-' + [i-12]).addClass('future');
     }
-
   };
